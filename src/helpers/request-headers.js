3f2a90ebf5481f726131cd7acbf6f030537c5821
Pierre Spring <pierre.spring@caillou.ch>
2019-03-07 10:33:16 +0100

    add cors headers

--- src/helpers/request-headers.js
+++ src/helpers/request-headers.js
@@ -3,6 +3,7 @@ module.exports = function requestHeaders (token) {
     Authorization: `Bearer ${token}`,
     'Log-Context': 'poc-web-components',
     'WSG-Credentials': process.env.JOURNEY_TOKEN_CLIENT_WSG_CREDENTIALS,
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
   }
 }
