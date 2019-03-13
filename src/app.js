1545ed6ed5619f4dba4e2334e61dd23b3965722d
Pierre Spring <pierre.spring@caillou.ch>
2019-03-13 13:12:49 +0100

    revert debugging

--- src/app.js
+++ src/app.js
@@ -1,20 +1,16 @@
 const app = require('express')()
 
-app.get('*', function (req, res) {
-  res.json(req.originalUrl)
-})
+app.use(require('./middleware/journey-token'))
+app.use(require('cors')())
 
-// app.use(require('./middleware/journey-token'))
-// app.use(require('cors')())
-//
-// app.get(
-//   '/station-search/:query',
-//   require('./controllers/stations-query')
-// )
-//
-// app.get(
-//   '/from/:from/to/:to',
-//   require('./controllers/trips-query')
-// )
+app.get(
+  '/station-search/:query',
+  require('./controllers/stations-query')
+)
+
+app.get(
+  '/from/:from/to/:to',
+  require('./controllers/trips-query')
+)
 
 module.exports = app
